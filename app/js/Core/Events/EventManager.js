1d524681f33b0aec1a91b939b0257ef005795906
Brendan Buono <brendan.buono@gmail.com>
2015-06-13 20:22:08 -0400

    Added unit test for fireEvent
    Unit tested fireEvent to make sure it firesEvents correctly. Also
    reformatted the files

--- app/js/Core/Events/EventManager.js
+++ app/js/Core/Events/EventManager.js
@@ -1,53 +1,53 @@
-(function(){
-  'use strict';
-  var Utility = require('../../Namespaces/Utility.js');
+(function() {
+ 'use strict';
+ var Utility = require('../../Namespaces/Utility.js');
 
-  function EventManager(){
-      this.registeredEventHandlers = [];
-      for(var i = 0;i<256;i++){
-        this.registeredEventHandlers[i] = [];
-      }
-      this.queuedEvents = new Utility.Queue();
-
+ function EventManager() {
+  this.registeredEventHandlers = [];
+  for (var i = 0; i < 256; i++) {
+   this.registeredEventHandlers[i] = [];
   }
-  EventManager.prototype = {
-     register : function(parent,handler,eventType){
-       handler.bind(parent);
-          this.registeredEventHandlers[eventType].push(handler);
-     },
-     unregister : function(handler,eventType){
-       var eventHandler = this.registeredEventHandlers[eventType];
-       var index = eventHandler.indexOf(handler);
-       eventHandler.splice(index,1);
-     },
-     fireEvent : function(ev){
-       var eventHandler = this.registeredEventHandlers[ev.EventType];
-       for(var i =0;i<eventHandler.length;i++){
-         eventHandler[i](ev);
-       }
-     },
-     queueEvent : function(ev){
-        this.queuedEvents.enqueue(ev);
-     },
-     update : function(maxMillis){
-       var startMillis = new Date().getTime();
-       for(var i =0;i<this.queuedEvents.size();i++){
-         var ev = this.queuedEvents.dequeue();
-         this.fireEvent(ev);
-         if(this._exceededTime(startMillis,maxMillis)){
-           break;
-         }
-       }
-     },
-     _exceededTime : function(startMillis, maxMillis){
-       var curMillis = new Date().getTime();
-       var elapsedMillis = curMillis - startMillis;
-       if(elapsedMillis > maxMillis){
-         return true;
-       }
-       return false;
-     }
-  };
+  this.queuedEvents = new Utility.Queue();
 
-  module.exports = EventManager;
+ }
+ EventManager.prototype = {
+  register: function(parent, handler, eventType) {
+   handler.bind(parent);
+   this.registeredEventHandlers[eventType].push(handler);
+  },
+  unregister: function(handler, eventType) {
+   var eventHandler = this.registeredEventHandlers[eventType];
+   var index = eventHandler.indexOf(handler);
+   eventHandler.splice(index, 1);
+  },
+  fireEvent: function(ev) {
+   var eventHandler = this.registeredEventHandlers[ev.EventType];
+   for (var i = 0; i < eventHandler.length; i++) {
+    eventHandler[i](ev);
+   }
+  },
+  queueEvent: function(ev) {
+   this.queuedEvents.enqueue(ev);
+  },
+  update: function(maxMillis) {
+   var startMillis = new Date().getTime();
+   for (var i = 0; i < this.queuedEvents.size(); i++) {
+    var ev = this.queuedEvents.dequeue();
+    this.fireEvent(ev);
+    if (this._exceededTime(startMillis, maxMillis)) {
+     break;
+    }
+   }
+  },
+  _exceededTime: function(startMillis, maxMillis) {
+   var curMillis = new Date().getTime();
+   var elapsedMillis = curMillis - startMillis;
+   if (elapsedMillis > maxMillis) {
+    return true;
+   }
+   return false;
+  }
+ };
+
+ module.exports = EventManager;
 }());
--- app/test/Core/Events/EventManager.test.js
+++ app/test/Core/Events/EventManager.test.js
@@ -1,64 +1,78 @@
-(function(){
-  'use strict';
-  var EventManager = require('../../../js/Core/Events/EventManager.js');
-  describe('EventManager', function(){
-    var eventManager = new EventManager();
-    beforeEach(function(){
-      eventManager = new EventManager();
-    });
-    describe('register',function(){
-      var parent = {};
-      var handler = function(){};
-      var eventType = 0;
-      beforeEach(function(){
-        spyOn(handler,'bind');
-
-      });
-        it('registers to correct event type',function(){
-          eventManager.register(parent,handler,eventType);
-          expect(eventManager.registeredEventHandlers[eventType].length).toBe(1);
-        });
-        it('registers the correct handler',function(){
-          eventManager.register(parent,handler,eventType);
-          expect(eventManager.registeredEventHandlers[eventType][0]).toBe(handler);
-        });
-        it('binds event handler to it\'s parent',function(){
-          eventManager.register(parent,handler,eventType);
-          expect(handler.bind).toHaveBeenCalledWith(parent);
-        });
-    });
-    describe('unregister',function(){
-      var mockRegisteredEventHandlers;
-      var eventType = 0;
-      var handlers = [1,2,3,4];
-      beforeEach(function(){
-       eventManager = new EventManager();
-       mockRegisteredEventHandlers = [];
-       mockRegisteredEventHandlers[eventType] = handlers;
-       eventManager.registeredEventHandlers = mockRegisteredEventHandlers;
-
-      });
-        it('unregisters handler',function(){
-          eventManager.unregister(2,eventType);
-          expect(eventManager.registeredEventHandlers[eventType]).toEqual([1,3,4]);
-        });
-    });
-    describe('queueEvent',function(){
-     beforeEach(function(){
-
-     });
-        it('queues single event',function(){
-          eventManager.queueEvent(1);
-          expect(eventManager.queuedEvents.peek()).toBe(1);
-        });
-        it('queues multiple events',function(){
-         for(var i =0;i<5;i++){
-          eventManager.queueEvent(i);
-         }
-         for(var j= 0;j<5;j++){
-          expect(eventManager.queuedEvents.dequeue()).toBe(j);
-         }
-        });
-    });
+(function() {
+ 'use strict';
+ var EventManager = require('../../../js/Core/Events/EventManager.js');
+ describe('EventManager', function() {
+  var eventManager = new EventManager();
+  beforeEach(function() {
+   eventManager = new EventManager();
   });
+  describe('register', function() {
+   var parent = {};
+   var handler = function() {};
+   var eventType = 0;
+   beforeEach(function() {
+    spyOn(handler, 'bind');
+
+   });
+   it('registers to correct event type', function() {
+    eventManager.register(parent, handler, eventType);
+    expect(eventManager.registeredEventHandlers[eventType].length).toBe(1);
+   });
+   it('registers the correct handler', function() {
+    eventManager.register(parent, handler, eventType);
+    expect(eventManager.registeredEventHandlers[eventType][0]).toBe(handler);
+   });
+   it('binds event handler to it\'s parent', function() {
+    eventManager.register(parent, handler, eventType);
+    expect(handler.bind).toHaveBeenCalledWith(parent);
+   });
+  });
+  describe('unregister', function() {
+   var mockRegisteredEventHandlers;
+   var eventType = 0;
+   var handlers = [1, 2, 3, 4];
+   beforeEach(function() {
+    eventManager = new EventManager();
+    mockRegisteredEventHandlers = [];
+    mockRegisteredEventHandlers[eventType] = handlers;
+    eventManager.registeredEventHandlers = mockRegisteredEventHandlers;
+
+   });
+   it('unregisters handler', function() {
+    eventManager.unregister(2, eventType);
+    expect(eventManager.registeredEventHandlers[eventType]).toEqual([1, 3, 4]);
+   });
+  });
+  describe('queueEvent', function() {
+   beforeEach(function() {
+
+   });
+   it('queues single event', function() {
+    eventManager.queueEvent(1);
+    expect(eventManager.queuedEvents.peek()).toBe(1);
+   });
+   it('queues multiple events', function() {
+    for (var i = 0; i < 5; i++) {
+     eventManager.queueEvent(i);
+    }
+    for (var j = 0; j < 5; j++) {
+     expect(eventManager.queuedEvents.dequeue()).toBe(j);
+    }
+   });
+  });
+  describe('fireEvent', function() {
+   var eventHandler;
+   var event = {
+    EventType: 0
+   };
+   beforeEach(function() {
+    eventHandler = jasmine.createSpy();
+    eventManager.registeredEventHandlers[event.EventType].push(eventHandler);
+   });
+   it('Calls the event handlers for the fired event', function() {
+    eventManager.fireEvent(event);
+    expect(eventHandler).toHaveBeenCalledWith(event);
+   });
+  });
+ });
 })();
